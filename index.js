e213329b0ee0f903858abcca0465a32a136eb10c
Henry Meehan <henryp.meehan@gmail.com>
2019-10-10 20:34:29 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,3 @@ return ("Happy holidays!") }
 function happyHolidaysTo(name) {
   return  ("Happy holidays, " +  `${name}!`)
 }
-
-function happyHolidayTo(holiday, name) {
-  return ("happy " + `${holiday}`",  " + `${name}!`)
-}
