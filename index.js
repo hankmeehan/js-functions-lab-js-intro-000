dc0c5e5a0e21040ae04f0d7b8cd1cb3f1fbb5f95
Henry Meehan <henryp.meehan@gmail.com>
2019-10-10 20:34:05 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -3,13 +3,9 @@ function happyHolidays(string) {
 return ("Happy holidays!") }
 
 function happyHolidaysTo(name) {
-  return "Happy holidays, " +  `${name}!`
+  return  ("Happy holidays, " +  `${name}!`)
 }
 
 function happyHolidayTo(holiday, name) {
-  return "happy " + `${holiday}`",  " + `${name}!`
-}
-
-function holidayCountdown(holiday, days){
-  return "It's " + `${days}`  + "days until " + `$(holiday)!`
+  return ("happy " + `${holiday}`",  " + `${name}!`)
 }
